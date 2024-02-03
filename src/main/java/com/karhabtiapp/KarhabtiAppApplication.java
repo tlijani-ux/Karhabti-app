88bd3f89b15dbfddf70ea64765aedd3a93591505
Seif Tlijani <114825087+tlijani-ux@users.noreply.github.com>
2024-02-03 09:42:06 +0100

    first commit

---
+++ src/main/java/com/karhabtiapp/KarhabtiAppApplication.java
@@ -0,0 +1,13 @@
+package com.karhabtiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarhabtiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarhabtiAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karhabtiapp/KarhabtiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karhabtiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarhabtiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
